a4c8b2b0fca8450fa1d70d373142dcb48c78a353
guilherme <vinespedro55@gmail.com>
2024-08-05 22:58:44 -0300

    feat: sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from "@phosphor-icons/react";
 
 export function Sidebar() {
 
@@ -6,13 +7,20 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
 
             <img className={styles.cover} src="https://plus.unsplash.com/premium_photo-1685086785636-2a1a0e5b591f?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        
+
+        
         <div className={styles.profile}>
-            <strong>Nome</strong>
-            <span>descrição</span>
+            <img className={styles.avatar} src="https://github.com/PedroGuilhermeSilv.png" alt="" />
+         
+            <strong>Pedro Guilherme</strong>
+            <span>Full Stack Developer</span>
         </div>
 
         <footer className={styles.footer}>
-            <a href="">Edite seu perfile</a>
+            
+            <a href="">
+            <PencilLine size={20} />Edite seu perfile</a>
         </footer>
         </aside>
     )
