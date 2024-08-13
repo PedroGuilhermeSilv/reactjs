9169b91a68dffbb81587683d8ceb8fc31d398bb6
guilherme <vinespedro55@gmail.com>
2024-08-13 20:31:06 -0300

    fix: commit

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,27 +1,28 @@
-import styles from './Sidebar.module.css'
+import { Avatar } from "./Avatar";
+import styles from "./Sidebar.module.css";
 import { PencilLine } from "@phosphor-icons/react";
 
 export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://plus.unsplash.com/premium_photo-1685086785636-2a1a0e5b591f?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
 
-    return(
-        <aside className={styles.sidebar}>
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/PedroGuilhermeSilv.png" alt="" />
 
-            <img className={styles.cover} src="https://plus.unsplash.com/premium_photo-1685086785636-2a1a0e5b591f?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        
+        <strong>Pedro Guilherme</strong>
+        <span>Full Stack Developer</span>
+      </div>
 
-        
-        <div className={styles.profile}>
-            <img className={styles.avatar} src="https://github.com/PedroGuilhermeSilv.png" alt="" />
-         
-            <strong>Pedro Guilherme</strong>
-            <span>Full Stack Developer</span>
-        </div>
-
-        <footer className={styles.footer}>
-            
-            <a href="">
-            <PencilLine size={20} />Edite seu perfile</a>
-        </footer>
-        </aside>
-    )
+      <footer className={styles.footer}>
+        <a href="">
+          <PencilLine size={20} />
+          Edite seu perfile
+        </a>
+      </footer>
+    </aside>
+  );
 }
