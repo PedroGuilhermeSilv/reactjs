68050500635a7f790e793e9a665765d417aa42e8
guilherme <vinespedro55@gmail.com>
2024-09-04 20:04:53 -0300

    feat: Header

---
+++ 02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import LogoIgnite from "../../assets/logo-ignite.svg";
+import { HeaderContainer } from "./style";
+
+export function Header() {
+    return (
+       < HeaderContainer>
+       <img src={LogoIgnite} alt="" />
+       <nav>
+        <NavLink to=""><Timer size={24}/></NavLink>
+        <NavLink to="/history"><Scroll size={24}/></NavLink>
+       </nav>
+       </HeaderContainer>
+    );
+}
